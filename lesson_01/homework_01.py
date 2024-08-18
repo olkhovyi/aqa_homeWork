9ce2bd2a4eaac24ef2f1252df8b74fa78cb9c431
olkhovyi <sander181984@gmail.com>
2024-08-18 17:04:33 +0300

    changes made

---
+++ lesson_01/homework_01.py
@@ -0,0 +1,82 @@
+# task 01 == Виправте синтаксичні помилки
+print("Hello", end = " ")
+print("world!")
+
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
+
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
+
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+
+# task 05 == виправте назви змінних
+side_1 = 1
+side_2 = 2
+side_3 = 3
+side_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = side_1 + side_2 + side_3 + side_4
+print(perimetery)
+
+
+"""
+    # Задачі 07 -10:
+    # Переведіть задачі з книги "Математика, 2 клас"
+    # на мову пітон і виведіть відповідь, так, щоб було
+    # зрозуміло дитині, що навчається в другому класі
+"""
+# task 07
+"""
+У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+Скільки всього дерев посадили в саду?
+"""
+apple = 4
+pear = apple + 5
+plum = apple - 2
+all_trees = apple + pear + plum
+print(all_trees)
+
+# task 08
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+am_temperature = 5
+pm_temperature = am_temperature - 10
+evening_temperature = pm_temperature +4
+print(evening_temperature)
+
+# task 09
+"""
+Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
+1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
+Скількі сьогодні дітей у театральному гуртку?
+"""
+boys = 24
+girls = boys / 2
+today_boys = boys - 1
+today_girls = girls - 2
+today_all_child = today_boys + today_girls
+print(today_all_child)
+# task 10
+"""
+Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
+а третя - як половина вартості першої та другої разом.
+Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
+"""
+first_book_price = 8
+second_book_price = first_book_price + 2
+third_book_price = (first_book_price + second_book_price) / 2
+all_book_price = first_book_price +second_book_price + third_book_price
+print(all_book_price)
