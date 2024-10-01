31f3904e98320afc20fdc15d47a0a9057c283347
Olkhovyi Oleksandr <88580012+olkhovyi@users.noreply.github.com>
2024-10-01 19:58:36 +0300

    Update homework_14.py

--- Lesson_14/homework_14.py
+++ Lesson_14/homework_14.py
@@ -8,14 +8,14 @@ class Student:
     # Method for changing the average score
     def change_average_grade(self, new_grade):
         self.average_grade = new_grade
-        print(f"Середній бал змінено на: {self.average_grade}")
+        print(f"Average score changed to: {self.average_grade}")
 
     # A method for outputting information about a student
     def display_info(self):
-        print(f"Ім'я: {self.name}")
-        print(f"Прізвище: {self.surname}")
-        print(f"Вік: {self.age}")
-        print(f"Середній бал: {self.average_grade}")
+        print(f"Name: {self.name}")
+        print(f"Surname: {self.surname}")
+        print(f"Age: {self.age}")
+        print(f"Average grade: {self.average_grade}")
 
 
 student = Student("Alex", "Jons", 18, 3.5)
@@ -26,5 +26,5 @@ student.display_info()
 student.change_average_grade(4.8)
 
 
-print("\nОновлена інформація про студента:")
+print("\nUpdated information about the student:")
 student.display_info()
