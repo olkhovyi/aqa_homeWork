0aad4d8ff0421575b2750c014e7c42024cee2a87
olkhovyi <sander181984@gmail.com>
2024-10-08 19:30:17 +0300

    added homework_15

--- Lesson_15/homework_15.py
+++ Lesson_15/homework_15.py
@@ -1,29 +1,32 @@
 class Rhombus:
     def __init__(self, side_a, angle_a):
-        self.setattr('side_a', side_a)
-        self.setattr('angle_a', angle_a)
+        self.side_a = side_a
+        self.angle_a = angle_a
 
-    def setattr(self, attr_name, value):
-        if attr_name == 'side_a':
+    # We override the __setattr__ method to control the setting of attributes
+    def __setattr__(self, name, value):
+        if name == 'side_a':
             if value <= 0:
                 raise ValueError("The side value must be greater than 0.")
-            self.__dict__[attr_name] = value
-        elif attr_name == 'angle_a':
-            if value <= 0 or value >= 180:
-                raise ValueError("The angle must be greater than 0 and less than 180 degrees.")
-            # Angle b is automatically calculated based on the fact that the sum of the angles must be equal to 180 degrees
-            angle_b = 180 - value
-            self.__dict__[attr_name] = value
-            self.__dict__['angle_b'] = angle_b
+            super().__setattr__(name, value)
+        elif name == 'angle_a':
+            if not (0 < value < 180):
+                raise ValueError("The angle must be in the range from 0 to 180 degrees.")
+            super().__setattr__(name, value)
+            super().__setattr__('angle_b', 180 - value)
         else:
-            raise AttributeError(f"Unknown attribute: {attr_name}")
+            super().__setattr__(name, value)
 
+    # A method for deriving information about a rhombus
     def get_info(self):
         return f"Side a: {self.side_a}, Angle α: {self.angle_a}, Angle β: {self.angle_b}"
 
 
 try:
-    rhombus = Rhombus(3, 40)
-    print(rhombus.get_info())
+    rhombus = Rhombus(5, 60)  #
+    print(rhombus.get_info())  #
 except ValueError as e:
     print(e)
+
+
+
